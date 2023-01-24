37750f350db6cb9c56913e8a8162d9e27760b8a9
Luca Candela <39223795+lucacandela@users.noreply.github.com>
2023-01-24 12:30:30 -0800

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,82 +1,82 @@
+import tkinter as tk
+import win32api
+import win32con
 import win32gui
-from time import sleep
-def winEnumHandler( hwnd, ctx ):
-    if win32gui.IsWindowVisible( hwnd ):
-        print ( hex( hwnd ), win32gui.GetWindowText( hwnd ) )
+from tkinter import messagebox
 
-def getWindow(title):
-    hwnd = win32gui.FindWindow(None, title)
+root = tk.Tk()
+root.title("Window Resizer")
+window_var = tk.StringVar()
 
-def getRenderChild(hwnd):
-    child_windows = win32gui.EnumChildWindows(hwnd,hwnd)
-
-    if not child_windows:
-        return None
-    
-
-
-def get_inner_windows(whndl):
-        def callback(hwnd, hwnds):
-            if win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
-                hwnds[win32gui.GetClassName(hwnd)] = hwnd
-            return True
-        hwnds = {}
-        win32gui.EnumChildWindows(whndl, callback, hwnds)
-        return hwnds
-
-def resize_child_window(wnd:int,w:int,h:int,is_emulator:bool = True):
-    if is_emulator:
-        x_off = 42
-        y_off = 35
+def resize_window():
+    if not window_var.get():
+        messagebox.showerror("Error", "No window selected.")
+        return
+    width_str = width_entry.get()
+    if width_str.isdigit():
+        width = int(width_str)
     else:
-        x_off = 6
-        y_off = 29
-    w = w+x_off
-    h = h+y_off
+        messagebox.showerror("Error","Width must be a number.")
+        return
+    height_str = height_entry.get()
+    if height_str.isdigit():
+        height = int(height_str)
+    else:
+        messagebox.showerror("Error","Height must be a number.")
+        return
+    height_entry.delete(0, 'end')
+    height_entry.insert(0, str(height))
+    hwnd = win32gui.FindWindow(None, window_var.get())
+    if hwnd:
+        left, top, _, _ = win32gui.GetWindowRect(hwnd)
 
-    x0, y0, _x1, _y1 = win32gui.GetWindowRect(wnd)
-
-    win32gui.MoveWindow(wnd,x0,y0,w,h,True)
-    win32gui.UpdateWindow(wnd)
-    print("Done")
-    sleep(3)
-        
-
-
-
-def main():
-    print("Make sure that the right-hand menu is fully expanded before starting. You may collapse it afterwards.")
-    #User input
-    wnd = input("What instance do you want re-sized? for? Type in the exact name -> ")
-    is_emu = True
-    hwnd = win32gui.FindWindow(None,wnd)
-    
-    if not hwnd:
-        print("The '{}' window doesn't exist... Try typing the name of your instance in exactly as it appears.".format(wnd))
-        
-        exit()
-    
+        win32gui.MoveWindow(hwnd, left, top, width, height, True)
+    else:
+        messagebox.showerror("Error", "The selected window was not found.")
 
 
-    windows = get_inner_windows(hwnd)
 
-    inner_render = windows.get("RenderWindow",None)
-    if not inner_render:
-        cont = input("The window has no render child... Do you want to continue? y/n -> ")
-        if cont != 'y':
-            print("Exiting.")
-            sleep(3)
-            exit()
-        else:
-            print("Continuing.")
-            is_emu = False
-    w = int(input("What width do you want to resize to? -> "))
-    h = int(input("What height do you want to resize to? -> "))
-    resize_child_window(hwnd,w,h,is_emulator=is_emu)
-    
-    print("wow arigato")
+# Get a list of all open windows on the taskbar
+windows = []
+windows.append([0,0])
+def enum_windows_callback(hwnd, lparam):
+    title = win32gui.GetWindowText(hwnd)
+    if title and win32gui.IsWindowVisible(hwnd):
+        windows.append((hwnd, title))
+win32gui.EnumWindows(enum_windows_callback, None)
+# Create a variable to store the selected window
+window_var = tk.StringVar(value=windows[0][1])
 
-if __name__ == '__main__':
-    main()
+# Create a dropdown list with all open windows
+window_list = tk.OptionMenu(root, window_var, *[text for _, text in windows])
+window_list.grid(row=0, column=0, columnspan=2)
 
+# Create a variable to store the selected ratio
+ratio_var = tk.StringVar(value="16:9")
 
+# Create a radio button for the 16:9 ratio
+ratio_16_9 = tk.Radiobutton(root, text="16:9", variable=ratio_var, value="16:9")
+ratio_16_9.grid(row=1, column=0)
+
+# Create a radio button for the 4:3 ratio
+ratio_4_3 = tk.Radiobutton(root, text="4:3", variable=ratio_var, value="4:3")
+ratio_4_3.grid(row=1, column=1)
+
+width_label = tk.Label(root, text="Width:")
+width_label.grid(row=2, column=0)
+
+width_entry = tk.Entry(root)
+width_entry.grid(row=2, column=1)
+
+height_label = tk.Label(root, text="Height:")
+height_label.grid(row=3, column=0)
+
+height_entry = tk.Entry(root)
+height_entry.grid(row=3, column=1)
+height_entry.config(state='disable')
+
+resize_button = tk.Button(root, text="Resize", command=resize_window)
+resize_button.grid(row=4, column=0, columnspan=2)
+
+offset_list = [{'Emulator':[42,35]}]
+root.mainloop()
