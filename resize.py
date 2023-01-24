e9afd6550c22fea980aeaf9e42d52cf8d31f0cfd
Luca Candela <39223795+lucacandela@users.noreply.github.com>
2023-01-24 14:52:44 -0800

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-import win32api
-import win32con
 import win32gui
 from tkinter import messagebox
+import sv_ttk
+from math import trunc
 
 root = tk.Tk()
 root.title("Window Resizer")
@@ -12,6 +12,7 @@ def resize_window():
     if not window_var.get():
         messagebox.showerror("Error", "No window selected.")
         return
+    
     width_str = width_entry.get()
     if width_str.isdigit():
         width = int(width_str)
@@ -23,12 +24,16 @@ def resize_window():
         height = int(height_str)
     else:
         messagebox.showerror("Error","Height must be a number.")
-        return
-    height_entry.delete(0, 'end')
-    height_entry.insert(0, str(height))
+
     hwnd = win32gui.FindWindow(None, window_var.get())
     if hwnd:
         left, top, _, _ = win32gui.GetWindowRect(hwnd)
+        offset_selection = offset_var.get()
+        for o in offsets:
+            if o[0] == offset_selection:
+                width = width + o[1][0]
+                height = height + o[1][1]
+                break
 
         win32gui.MoveWindow(hwnd, left, top, width, height, True)
     else:
@@ -38,7 +43,6 @@ def resize_window():
 
 # Get a list of all open windows on the taskbar
 windows = []
-windows.append([0,0])
 def enum_windows_callback(hwnd, lparam):
     title = win32gui.GetWindowText(hwnd)
     if title and win32gui.IsWindowVisible(hwnd):
@@ -47,38 +51,72 @@ win32gui.EnumWindows(enum_windows_callback, None)
 # Create a variable to store the selected window
 window_var = tk.StringVar(value=windows[0][1])
 
+window_label = tk.Label(root, text="Window:")
+window_label.grid(row=0, column=0)
+
 # Create a dropdown list with all open windows
 window_list = tk.OptionMenu(root, window_var, *[text for _, text in windows])
-window_list.grid(row=0, column=0, columnspan=2)
+window_list.grid(row=0, column=1, columnspan=2)
 
+
+ratio_label = tk.Label(root, text="Ratio:")
+ratio_label.grid(row=1, column=0)
 # Create a variable to store the selected ratio
-ratio_var = tk.StringVar(value="16:9")
+ratios = ["16:9","4:3","9:16"]
+ratio_var = tk.StringVar(value=ratios[0])
+ratio_var.trace("w", lambda name, index, mode, ratio_var=ratio_var: getRatioLockedHeight())
+ratio_list = tk.OptionMenu(root, ratio_var, *[text for text in ratios])
+ratio_list.grid(row=1,column=1,columnspan=2)
 
-# Create a radio button for the 16:9 ratio
-ratio_16_9 = tk.Radiobutton(root, text="16:9", variable=ratio_var, value="16:9")
-ratio_16_9.grid(row=1, column=0)
 
-# Create a radio button for the 4:3 ratio
-ratio_4_3 = tk.Radiobutton(root, text="4:3", variable=ratio_var, value="4:3")
-ratio_4_3.grid(row=1, column=1)
 
 width_label = tk.Label(root, text="Width:")
 width_label.grid(row=2, column=0)
 
-width_entry = tk.Entry(root)
+def getRatioLockedHeight():
+    height_entry.config(state='normal')
+    r = ratio_var.get()
+    w_str = width_entry.get()
+    if w_str.isdigit():
+        w = int(w_str)
+        height_entry.delete(0,'end')
+        ratio = r.split(":")
+        height_entry.insert(0,trunc(w * (int(ratio[1])/int(ratio[0]))))
+    else:
+        height_entry.delete(0,'end')
+        height_entry.insert(0,"Width is not an int") 
+    height_entry.config(state='readonly',fg='black')
+    
+
+width_var = tk.StringVar()
+width_entry = tk.Entry(root, textvariable=width_var)
 width_entry.grid(row=2, column=1)
+width_var.trace_add("write", lambda name, index, mode, width_var=width_var: getRatioLockedHeight())
 
 height_label = tk.Label(root, text="Height:")
 height_label.grid(row=3, column=0)
 
+
+height_var = 0
 height_entry = tk.Entry(root)
 height_entry.grid(row=3, column=1)
-height_entry.config(state='disable')
+height_entry.config(state='readonly',fg='black')
+
+
+offset_label = tk.Label(root,text="Offsets:")
+offset_label.grid(row=4,column=0)
+
+#create a varialbe to store the offset list
+offsets = [('None',[0,0]),('Emulator',[42,35]),('Genshin',[6,29])]
+offset_var = tk.StringVar(value=offsets[0][0])
+offset_list = tk.OptionMenu(root, offset_var, *[tu for tu, text in offsets])
+offset_list.grid(row=4, column=1,columnspan=2)
 
 resize_button = tk.Button(root, text="Resize", command=resize_window)
-resize_button.grid(row=4, column=0, columnspan=2)
+resize_button.grid(row=6, column=0, columnspan=2)
 
-offset_list = [{'None':[0,0],'Emulator':[42,35],'Genshin':[6,29]}]
 
+
+sv_ttk.set_theme("dark")
 
 root.mainloop()
